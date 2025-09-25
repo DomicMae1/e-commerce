5538576c358406473cd207d3cc75edc46c2bdb0b
DomicMae1 <ardon.yunors@tako.co.id>
2025-09-25 14:11:50 +0700

    fix(product): fix calculation logic for product pricing

--- src/app/produk/[id]/page.tsx
+++ src/app/produk/[id]/page.tsx
@@ -3,8 +3,9 @@ import { notFound } from "next/navigation";
 import { products } from "@/data/products";
 import ProductDetailClient from "./ProductDetailClient";
 
-export default function ProductDetailPage({ params }: any) {
-  const product = products.find((p) => p.id.toString() === params.id);
+export default async function ProductDetailPage({ params }: any) {
+  const { id } = await params; // ⬅️ params harus di-await
+  const product = products.find((p) => p.id.toString() === id);
 
   if (!product) {
     notFound();
